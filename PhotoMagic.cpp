f67061562e3758a210555fbf38d3a2b4d93d010d
Daniel Dsouza <daniel_dsouza@student.uml.edu>
2025-01-23 10:28:54 -0500

    PhotoMagic.cpp

---
+++ PhotoMagic.cpp
@@ -0,0 +1,27 @@
+// Copyright 2024 Daniel Dsouza
+#include "PhotoMagic.hpp"
+
+namespace PhotoMagic {
+  void transform(sf::Image& image, FibLFSR* lfsr) {
+    sf::Vector2u size = image.getSize();
+
+    for (unsigned y = 0; y < size.y; y++) {
+      for (unsigned x = 0; x < size.x; x++) {
+        sf::Color pixel = image.getPixel(x, y);
+
+        // Generateing a new 8-bit integer for each color component.
+        int redRand = lfsr->generate(8);
+        int greenRand = lfsr->generate(8);
+        int blueRand = lfsr->generate(8);
+
+        // XOR each color component with the newly-generated 8-bit integer.
+        pixel.r ^= redRand;
+        pixel.g ^= greenRand;
+        pixel.b ^= blueRand;
+
+        // Setting the pixel in the image to the new color
+        image.setPixel(x, y, pixel);
+      }
+    }
+  }
+}  // namespace PhotoMagic
